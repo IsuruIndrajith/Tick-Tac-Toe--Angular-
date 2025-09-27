4a70769d8b5d13f533558a7ed1d64e895e5ebb30
Isuru <isuruindrajith680@gmail.com>
2025-09-27 09:50:58 +0530

    ng-components | ng-header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  template:
+  `<app-header></app-header>
+  <app-home></app-home>
+  <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'TickTacToe';
